cf1ab223991c43de4d8a01ebe74003945db4812a
AbaiKeks <fghf09467@gmail.com>
2023-01-30 19:56:46 +0600

    dz 3 js

---
+++ main.js
@@ -0,0 +1,46 @@
+//  Home Task part1
+
+//  function js3(a,b){
+//    if (a<b ) 
+//    alert('Второе число больше первого!');
+//   else if (a>b)
+//   alert(' первое число больше второгоого!');
+//  }
+//  js3(4,3)
+
+
+
+// Home Task part2
+
+
+// function twoMass(){
+//      let arr1 = prompt ('введите первое слово')
+//      let arr2 = prompt ('введите второе слово')
+//     if (arr1 > arr2)
+//     alert('первое слово больше второго ');
+//     else if (arr1 < arr2 )
+//     alert ('второе слово больше первого');
+// }
+// twoMass()
+     
+
+// Home Task part3
+
+// function CountChar (){
+//     let a = prompt('введите слово  ')
+//     alert ('введенное слово имеет : ' + a.length + ' кодовых значений')
+// }
+//   CountChar()
+
+ 
+
+// Home task extra
+
+
+
+// function abai (a,b,c){
+//  return (a + b + c );
+ 
+// }
+// let some_test = abai(1,2,3)
+// console.log(some_test)
